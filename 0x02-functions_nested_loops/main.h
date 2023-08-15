5cbd863e882536772212322a4f3767fbb59f738b
Luyolo <luyolo.tuta23@gmail.com>
2023-08-15 12:12:56 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,2 @@
+VimCrypt~03!¯wneØNà¹:GµÿÝÜ_0C5Áðæ}
+ôØšÝòs^®Š+•¼XB¶%ß3ã¡ó€F	#õ}ù’­e¡`)åÌ±Iá¹;ÀØ6—â 2¸d®æ¨Äßdû=øñ $5Ëò9B¡¡¤v›pT¸+Í–üÒÇ$ÊÏ$×Ç?ÂlR)nŽÊ/}²qÁ’ˆýÃï}©ký‚ƒ–/ü{q|Ê¯ÉQøTý‡?‹UOPäiÀog-øÚ†8t€k‘=É|M­û¬†ö&$•Õ»eŽ‰»ï#àfS,‡ë˜r;:W4ä­¸®šKý=¬	‡õaÎ°v^SRH-K¶¿û›ÿÓ¡{Œ5ïÄÅ¯ðê]²ïù±hDSª]¯`æ` ‘rc¹ÿµŒ	+’}My”]¥i„ô‡ìð!jkc4·ÜÖ…­¯›aÑÇ
