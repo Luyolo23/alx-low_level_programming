644e257eda228ad3b1dfce906701124cca935ef6
Luyolo23 <luyolo.tuta23@gmail.com>
2024-01-16 00:22:53 -0800

    create a dynamic library

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: wanted value
+ * @src: wanted value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+int y;
+int z;
+y = 0;
+while (dest[y] != '\0')
+{
+y++;
+}
+z = 0;
+while (src[z] != '\0')
+{
+dest[y] = src[z];
+y++;
+z++;
+}
+dest[y] = '\0';
+return (dest);
+}
